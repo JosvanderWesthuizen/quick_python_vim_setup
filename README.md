45b599abf962f36f1f66303a7d83dac22a2f9dcb
josvdwest <josvdwest@gmail.com>
2019-11-04 22:03:25 +0000

    untested changes

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 1. Clone this repo and go navigate to the repo folder in your terminal.
 
 2. Run `bash setup.sh`
+
+## This setup has been tested only on Ubuntu 18.04
+
+It won't work on a Mac
--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir ~/.vim/colors
 mv distinguished.vim ~/.vim/colors/
 vim -c 'PluginInstall' -c 'qa!'
+
+apt install flake8
